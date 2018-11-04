bce7315d570a91b330a1430df23153149200b577
Zohre-Sooratian <zohre.sooratian@gmail.com>
2018-11-04 11:49:09 -0500

    first commit

---
+++ zwebsite/js/script.js
@@ -0,0 +1,94 @@
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+document.getElementById("k1").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k1").addEventListener("mouseout", function() {
+   document.body.style.background = "#D9EAFF";
+}, false);
+
+document.getElementById("k2").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k2").addEventListener("mouseout", function() {
+   document.body.style.background = "#D9EAFF";
+}, false);
+
+document.getElementById("k3").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k3").addEventListener("mouseout", function() {
+   document.body.style.background = "#D9EAFF";
+}, false);
+
+document.getElementById("k4").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k4").addEventListener("mouseout", function() {
+   document.body.style.background = "#D9EAFF";
+}, false);
+
+document.getElementById("k5").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k5").addEventListener("mouseout", function() {
+   document.body.style.background = "#D9EAFF";
+}, false);
+
+document.getElementById("k6").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k6").addEventListener("mouseout", function() {
+   document.body.style.background = "#D9EAFF";
+}, false);
+
+document.getElementById("k7").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k7").addEventListener("mouseout", function() {
+   document.body.style.background = "#A5FFDA";
+}, false);
+
+document.getElementById("k8").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k8").addEventListener("mouseout", function() {
+   document.body.style.background = "#A5FFDA";
+}, false);
+
+document.getElementById("k9").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k9").addEventListener("mouseout", function() {
+   document.body.style.background = "#A5FFDA";
+}, false);
+
+document.getElementById("k10").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k10").addEventListener("mouseout", function() {
+   document.body.style.background = "#A5FFDA";
+}, false);
+
+document.getElementById("k11").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k11").addEventListener("mouseout", function() {
+   document.body.style.background = "#A5FFDA";
+}, false);
+
+document.getElementById("k12").addEventListener("mouseover", function() {
+  document.body.style.background = "#87F2C6";
+}, false);
+document.getElementById("k12").addEventListener("mouseout", function() {
+   document.body.style.background = "#A5FFDA";
+}, false);
